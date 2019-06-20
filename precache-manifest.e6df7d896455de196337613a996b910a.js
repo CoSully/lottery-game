1566b14be5527621df12cb07d86c446c2fdb7890
Chris OSullivan <CoSully@gmail.com>
2019-06-20 18:51:32 +0100

    Updates

---
+++ precache-manifest.e6df7d896455de196337613a996b910a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a80178f161fba7729db3d0afc431ef",
+    "url": "/lottery-game/index.html"
+  },
+  {
+    "revision": "69fa3f2b7de3d70a9453",
+    "url": "/lottery-game/static/css/main.aa5a2dfe.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/lottery-game/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "69fa3f2b7de3d70a9453",
+    "url": "/lottery-game/static/js/main.e9963879.chunk.js"
+  },
+  {
+    "revision": "e9d177bf5d1a1fb9871a",
+    "url": "/lottery-game/static/js/runtime~main.bdc9506f.js"
+  }
+]);
